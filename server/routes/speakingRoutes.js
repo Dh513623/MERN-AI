fc5b689bb7fa2c3c7f76b37a3989ddebb47f3ead
Rajadharshini24 <23cseb22.rajadharshini.r@gmail.com>
2026-06-18 14:03:30 +0530

    Refactor speaking route to remove file upload
    Removed multer upload middleware from speaking route.

--- server/routes/speakingRoutes.js
+++ server/routes/speakingRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
+
 
 const { speakingEvaluate } = require("../controllers/speakingController");
 const verifyToken=require('../middleware/authMiddleware');
 
-const upload = multer({ dest: "uploads/" });
 
 // Single route to handle both modes
-router.post("/", verifyToken, upload.single("audio"), speakingEvaluate);
+router.post("/", verifyToken, speakingEvaluate);
 
 module.exports = router;
